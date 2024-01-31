ffd97eea5133f51e93c013d5183742d82d915dbf
Seo-eun <151019751+seoeun9595@users.noreply.github.com>
2024-01-31 23:02:54 +0900

    Title: 공백 넣어서 출력, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240131/공백 넣어서 출력/insert-space-and-print.py
@@ -0,0 +1,4 @@
+print("score          subject          name\n")
+print("  100             math           sam")
+print("   88          science          paul")
+print("   96         language          jack")
