bec18436640c739eddbd718f09edecca46bc7bd9
Seo-eun <151019751+seoeun9595@users.noreply.github.com>
2024-02-01 21:32:38 +0900

    Title: 순서대로 구구단, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240201/순서대로 구구단/multiplication-table-in-order.py
+++ 240201/순서대로 구구단/multiplication-table-in-order.py
@@ -1,4 +1,12 @@
 num1, num2 = map(int, input().split())
 
-for i in range(1, 10):
-    print(f"{num1} * {i} = {num1*i}  {num2} * {i} = {num2*i}")
+if num1 <= num2:
+    for i in range(1, 10):
+        for j in range(num1, num2+1):
+            print(f"{j} * {i} = {j*i}", end = '  ')
+        print("")
+else:
+    for i in range(1, 10):
+        for j in range(num1, num2-1, -1):
+            print(f"{j} * {i} = {j*i}", end = '  ')
+        print("")
