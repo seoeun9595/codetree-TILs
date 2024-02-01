6c7a978cdfbae6891edc79fe2d360faf936d660e
Seo-eun <151019751+seoeun9595@users.noreply.github.com>
2024-02-01 21:21:33 +0900

    Title: 윤년, Time: 168ms, Memory: 24MB, Status: Passed - CodeTree

--- 240201/윤년/leap-year.py
+++ 240201/윤년/leap-year.py
@@ -1,9 +1,8 @@
 year = int(input())
 
-if year % 4 == 0:
-    if year % 100 != 0:
-        print(0)
-    elif year % 400 == 0:
-        print(1)
+if year % 4 == 0 and year % 100 != 0:
+    print(1)
+elif year % 400 == 0:
+    print(1)
 else:
     print(0)
